a0c1f49cdf6dd6b43d9c882a52003e05a56e3ccf
shaurya Negi <creatorsb2358@gmail.com>
2026-07-01 15:16:26 +0530

    done with my second question

--- demo.cpp
+++ demo.cpp
@@ -1,13 +1,15 @@
-// 1. question of pattern ;
+// 2. question of pattern ;
 
 #include <iostream>
 using namespace std;
 
 int main(){
     int n = 5;
-    for(int i = 1 ; i <= n; i++){
-        for(int j = 0; j < n ; j++ ){
-            cout << "*";
+    for(int i = 1; i <= n ; i++){
+        int num = 1;
+        for(int j = 0; j < i; j++){
+            cout << num ;
+            num++;
         }
         cout << endl;
     }
