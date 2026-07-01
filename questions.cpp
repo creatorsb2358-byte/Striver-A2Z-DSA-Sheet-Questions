e582608f6a027f5f4f012095e31f0561c0b92239
shaurya Negi <creatorsb2358@gmail.com>
2026-07-01 16:25:21 +0530

    done with my 20 butterfly pattern question

--- questions.cpp
+++ questions.cpp
@@ -1,34 +1,33 @@
-// 19. question in pattern;
+// 20. question in pattern;
 
 #include <iostream>
 using namespace std;
 
 int main(){
-    int n =  5;
-    for(int i = 0;i< n; i++ ){
-        for(int j = 0; j < n-i; j++){
-            cout << "*";
-        }
-        for(int j = 0; j<2*i; j++){
-            cout << " ";
-        }
-        for(int j = 0; j<n-i; j++){
-            cout << "*";
-        }
-        cout << endl;
-    }
-    int m =5 ;
-    for(int i = 1 ; i<=m ; i++ ){
-        for(int j = 0; j < i; j++){
+    int n = 5;
+    for(int i = 1; i<= n; i++){
+        for(int j = 0; j < i;j++){
             cout << "*";
         }
         for(int j = 0; j<2*(n-i); j++){
             cout << " ";
         }
-        for (int j = 0; j< i; j++){
+        for(int j=0; j<i; j++){
             cout << "*";
         }
         cout << endl;
-
+    }
+    int m = 5;
+    for(int i= 0; i< m; i++){
+        for(int j =0; j<m-i; j++){
+            cout << "*";
+        }
+        for(int j = 0; j<2*i ; j++){
+            cout << " ";
+        }
+        for(int j =0; j<m-i; j++){
+            cout << "*";
+        }
+        cout << endl;
     }
 }
