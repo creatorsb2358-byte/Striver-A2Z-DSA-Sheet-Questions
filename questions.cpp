9b962cf644d7a9416d22ef00b3f35eaaca60d529
shaurya Negi <creatorsb2358@gmail.com>
2026-07-14 10:21:43 +0530

    to print the subarray

--- questions.cpp
+++ questions.cpp
@@ -1,19 +1,17 @@
-///linear search in vector:
+///to print the subarray:
 
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int linear_search(vector<int>& nums, int target){
-    for(int i : nums){
-        if(target == nums[i]){
-            cout << i;
-        }
-    }
-}
-
 int main(){
-    vector<int> numm = {1,2,3,4,5};
-    int target = 4;
-    linear_search(numm , target);
+    int arr[5] = {1,2,3,4,5};
+    for(int st = 0; st < 5; st++){
+        for(int end = st; end < 5 ; end++){
+            for(int i = st; i <= end; i++){
+                cout << arr[i] ;
+            }
+            cout << " " ;
+        }
+        cout << endl;
+    }
 }
