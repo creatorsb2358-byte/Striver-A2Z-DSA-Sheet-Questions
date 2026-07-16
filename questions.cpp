b092f0c5817bc360ca415ac37739f2a97e4f553f
shaurya Negi <creatorsb2358@gmail.com>
2026-07-16 22:57:10 +0530

    sort array by parity

--- questions.cpp
+++ questions.cpp
@@ -1,4 +1,4 @@
-///move zeroes:
+///sort array by parity:
 
 #include <iostream>
 #include <vector>
@@ -6,16 +6,24 @@ using namespace std;
 
 int main(){
     vector<int> nums = {0,1,2,0,4,5};
-    int size = nums.size();
-        int st = 0, end = 0;
-        for(int i = 0 ; i<size;i++){
-            if(nums[end] != 0){
-                swap(nums[st],nums[end]);
+        int size = nums.size();
+        int st = 0, end = 1;
+        while(end < size ){
+            if(nums[st] % 2 == 0 && nums[end] % 2 != 0){
                 st++;
                 end++;
-            }else if(nums[end] == 0){
+           
+            }else if(nums[st] % 2 == 0 && nums[end] % 2== 0){
+                end++;
+                st++;
+            }else if(nums[st] % 2 != 0 && nums[end] % 2 == 0){
+                swap(nums[st],nums[end]);
+                st++;
+                end = st+1;
+            }else if(nums[st] % 2 != 0 && nums[end] % 2 != 0){
                 end++;
             }
+
         }
         for(int i : nums){
             cout << i;
