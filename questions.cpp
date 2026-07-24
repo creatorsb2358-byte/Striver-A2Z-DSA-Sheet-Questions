9b9fed8ea9b10efecd8e3ec9817d405e09567946
shaurya Negi <creatorsb2358@gmail.com>
2026-07-24 20:47:10 +0530

    count total mountains and valleys

--- questions.cpp
+++ questions.cpp
@@ -1,24 +1,26 @@
-///monotonic array:
+///count total mountain and valleys:
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int main(){
-    vector<int> nums = {1,2,3,4};
-    int size = nums.size();
-    bool isincreasing = false , isdecreasing = false;
-    for(int i = 1 ;i < size ;i++){
-        if(nums[i] > nums[i-1]){
-            isincreasing = true ;
-        }else if(nums[i] < nums[i-1]){
-            isdecreasing = true;
+    vector<int> nums = {1,2,3,2,5};
+    int count = 0;
+    int left = 0;
+
+    for (int i = 1; i < nums.size() - 1; ++i) {
+        if (nums[i] != nums[i + 1]) {
+            if ((nums[i] > nums[left] && nums[i] > nums[i + 1]) ||
+                (nums[i] < nums[left] && nums[i] < nums[i + 1])) {
+                ++count;
+            }
+            left = i;
+                
         }
     }
-    if(isincreasing == true && isdecreasing == true){
-        return false;
-    }else{
-        return true;
-    }
+
+    cout << count;
+
 }
 
