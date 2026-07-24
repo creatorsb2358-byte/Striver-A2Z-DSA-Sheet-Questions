c6a7ac53fcf100114d785d2b6d841e3aa2aed69f
shaurya Negi <creatorsb2358@gmail.com>
2026-07-24 11:32:49 +0530

    monotonic array

--- questions.cpp
+++ questions.cpp
@@ -1,22 +1,24 @@
-///find the xor of numbers which appear twice:
+///monotonic array:
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int main(){
-    vector<int> nums = {7,1,3,5,6,3,6};
+    vector<int> nums = {1,2,3,4};
     int size = nums.size();
-    int ans = 0;
-    for(int i = 0; i< size ; i++){
-        for(int j = i+1; j< size ; j++){        
-            if(nums[i] == nums[j]){
-                ans ^= nums[i]; 
-                break;
-            }
+    bool isincreasing = false , isdecreasing = false;
+    for(int i = 1 ;i < size ;i++){
+        if(nums[i] > nums[i-1]){
+            isincreasing = true ;
+        }else if(nums[i] < nums[i-1]){
+            isdecreasing = true;
         }
-        
     }
-    cout << ans;     
+    if(isincreasing == true && isdecreasing == true){
+        return false;
+    }else{
+        return true;
+    }
 }
 
